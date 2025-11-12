4c1a67a61fe40418b584710f2a7e8d969981078f
culad-a <alexisdulac.mail@gmail.com>
2025-11-12 18:50:35 +0100

    balise google ads test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import CookieConsent from "@/components/CookieConsent";
@@ -70,6 +71,20 @@ export default function RootLayout({
         </footer>
         <Toaster />
         <CookieConsent />
+
+        {/* Google Tag Manager */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-10989656220"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-10989656220');
+          `}
+        </Script>
       </body>
     </html>
   );
